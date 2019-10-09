5e5e806e531bfe89568f7f61fa8b485b8c0106c8
Pirino, Wil <wil.pirino@transamerica.com>
2019-10-08 21:42:54 -0600

    checking files in root of src dir when linting

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,13 +1,11 @@
 import { Router } from 'express';
 
-import UserRouter from './Users';
-import DonationsRouter from './Donations';
+import DonationsRouter from './donations';
 
 // Init router and path
 const router = Router();
 
 // Add sub-routes
-router.use('/users', UserRouter);
 router.use('/donations', DonationsRouter);
 
 // Export the base-router
