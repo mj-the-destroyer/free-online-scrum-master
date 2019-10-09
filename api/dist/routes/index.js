33d439edda9faa9a193a3aeb87a4e29d27c96c63
Pirino, Wil <wil.pirino@transamerica.com>
2019-10-09 15:48:21 -0600

    .gitignore fix

--- api/dist/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const express_1 = require("express");
-const donations_1 = tslib_1.__importDefault(require("./donations"));
-const router = express_1.Router();
-router.use('/donations', donations_1.default);
-exports.default = router;
